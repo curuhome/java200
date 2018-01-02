b9da7eaa445b5c941915743d11dafa42e0756811
curuhome <curuhome@yahoo.co.jp>
2018-01-02 20:20:06 +0900

    chap086

---
+++ src/xyz/curuhome/chap086/LeapYearTotalDaysMain.java
@@ -0,0 +1,10 @@
+package xyz.curuhome.chap086;
+
+public class LeapYearTotalDaysMain {
+    public static void main(String[] args)
+    {
+        LeapYearTotalDays leap = new LeapYearTotalDays();
+        System.out.println(leap.getTotalDays(2004));
+        System.out.println(leap.getTotalDays(2005));
+    }
+}
