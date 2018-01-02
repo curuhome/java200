98e012a681d41f7ecf7950143708e2990f01a615
curuhome <curuhome@yahoo.co.jp>
2018-01-02 19:56:08 +0900

    chap086

--- src/xyz/curuhome/chap086/IfLeapYear2Main.java
+++ src/xyz/curuhome/chap086/IfLeapYear2Main.java
@@ -4,12 +4,15 @@ public class IfLeapYear2Main {
     public static void main(String[] args) {
         IfLeapYear2 leap = new IfLeapYear2();
 
-        leap.setYear(2004);//member 필드를 2004로
+        leap.setYear(2004); // member 필드를 2004로 대입
         System.out.println(leap.isLeapYear());
-        leap.prints();     //2004년 윤년여부
+        /* true */
+        leap.prints();      // 2004년 윤년여부(true)
+        /* 2004는 윤년입니다.  */
 
-        leap.setYear(2005);//member 필드를 2005로
+        leap.setYear(2005); // member 필드를 2005로 대입
 //        System.out.println(leap.isLeapYear());
-        leap.prints();     //2005년 윤년여부
+        leap.prints();      // 2005년 윤년여부(false)
+        /* 2005는 윤년이 아닙니다. */
     }
 }
