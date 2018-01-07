367d5e74faca96b56c0f934fedb7b2dbb0d0357e
curuhome <curuhome@yahoo.co.jp>
2018-01-07 18:44:32 +0900

    chap100

---
+++ src/xyz/curuhome/chap100/StringEqualMain.java
@@ -0,0 +1,43 @@
+package xyz.curuhome.chap100;
+
+public class StringEqualMain {
+    public static void main(String[] args) {
+        //String literal
+        String st1 = "Hello Jungbo!";
+        String st2 = "Hello Jungbo!";
+        String st3 = "Go Jungbo!";
+
+        //String Object
+        String sr1 = new String("Hello Jungbo!");
+        String sr2 = new String("Hello Jungbo!");
+        String sr3 = new String("GO GO Jungbo!");
+        String sr4 = sr1;
+
+        System.out.println("1  st1==st2 :"+(st1==st2));
+        /* 1  st1==st2 :true */
+        System.out.println("2  st1.equals(st2) :"+(st1.equals(st2)));
+        /* 2  st1.equals(st2) :true */
+        System.out.println("3  st1==st3 : "+(st1==st3));
+        /* 3  st1==st3 : false */
+        System.out.println("4  st1.equals(st3) : "+(st1.equals(st3)));
+        /* 4  st1.equals(st3) : false */
+
+        System.out.println("5  sr1==sr2 : "+(sr1==sr2));
+        /* 5  sr1==sr2 : false */
+        System.out.println("6  sr1.equals(sr2) : "+(sr1.equals(sr2)));
+        /* 6  sr1.equals(sr2) : true */
+        System.out.println("7  sr1==sr3 : "+(sr1==sr3));
+        /* 7  sr1==sr3 : false */
+        System.out.println("8  sr1.equals(sr3) : "+(sr1.equals(sr3)));
+        /* 8  sr1.equals(sr3) : false */
+
+        System.out.println("9  st1==sr1 : "+(st1==sr1));
+        /* 9  st1==sr1 : false */
+        System.out.println("10 st1.equals(sr1) : "+(st1.equals(st2)));
+        /* 10 st1.equals(sr1) : true */
+        System.out.println("11 sr1==sr4 : "+(sr1==sr4));
+        /* 11 sr1==sr4 : true */
+        System.out.println("12 sr1.equals(sr4) : "+(sr1.equals(sr4)));
+        /* 12 sr1.equals(sr4) : true */
+    }
+}
