fdea80f450f5350a92ee2343a7281d2fde2f0915
curuhome <curuhome@yahoo.co.jp>
2018-01-07 19:30:29 +0900

    chap101

---
+++ src/xyz/curuhome/chap101/CalendarMain.java
@@ -0,0 +1,13 @@
+package xyz.curuhome.chap101;
+
+public class CalendarMain {
+    public static void main(String[] args) {
+        CalendarIn7 cal7 = new CalendarIn7();
+        int year=2018;
+        for(int i=1;i<13;i++){
+            System.out.println("\t\t"+year+"년\t"+i+"월\n");
+            cal7.printCalendar(year,i);
+            System.out.println("\n---------------------------------------------------");
+        }
+    }
+}
