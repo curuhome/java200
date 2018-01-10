124dc89ca86c9c51075dfdfc83ec5966897afadf
curuhome <curuhome@yahoo.co.jp>
2018-01-10 17:35:17 +0900

    chap113

--- src/xyz/curuhome/chap113/PointRotateMain.java
+++ src/xyz/curuhome/chap113/PointRotateMain.java
@@ -8,8 +8,10 @@ public class PointRotateMain {
         Point p1 = new Point(4,4);
         Point p2 = ro.rotate(p1,90);//90도 회전
         System.out.println(p2);
+        /* (-3.9999999999999996 , 4.0) */
 
         Point p3 = ro.rotate(p1,45);//45도 회전
         System.out.println(p3);
+        /* (4.440892098500626E-16 , 5.65685424949238) */
     }
 }
