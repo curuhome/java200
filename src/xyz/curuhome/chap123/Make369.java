bf271b603b0bdc67c8103ea32c37e3c9a2452bc8
curuhome <curuhome@yahoo.co.jp>
2018-01-20 21:52:54 +0900

    chap123

---
+++ src/xyz/curuhome/chap123/Make369.java
@@ -0,0 +1,34 @@
+package xyz.curuhome.chap123;
+
+public class Make369 {
+    private int number369(int number){
+        int a;
+        int num=0;
+        a = number;
+        for(int i=0;i<length(number);i++){
+            if((a%10)!=0 && (a%10)%3==0){//0이 아니면서 3,6,9
+                num++;
+            }
+            a = a/10;
+        }
+        return num;
+    }
+    private int length(int nums){
+        return (nums+"").length();
+    }
+    public void play369(int max){
+        for(int i=1;i<=max;i++){
+            if(number369(i)>0){
+                for(int j=1;j<=number369(i);j++){
+                    System.out.print("*");
+                }
+                System.out.print("\t");
+            }else{
+                System.out.print(i+"\t");
+            }
+            if(i%10==0){
+                System.out.println();
+            }
+        }
+    }
+}
